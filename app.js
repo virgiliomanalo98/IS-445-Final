9e1f0f67b4f68d6003ae370ffe4492045cbc013e
Virgil Manalo <virgilio.manalo98@gmail.com>
2020-05-12 21:26:25 -0700

    Step 3 Commit 4

--- app.js
+++ app.js
@@ -5,20 +5,24 @@ function evenNum(){
     // check if the numbers fit criteria
     if (number1 <= 2 || number1 >= 100){
         console.log(parseInt(number1,10));
-        document.getElementById("msg").innerHTML += "Number 1 input" + number1 + " is not a valid number.";
+        document.getElementById("msg1").innerHTML = "Number 1 input" + number1 + " is not a valid number.";
         document.getElementById("result").innerHTML = "";
-        document.getElementById("msg").style.color="red";
+        document.getElementById("msg1").style.color="red";
         document.getElementById("result").style.color="red";
-        
+        document.getElementById("array").innerHTML="";
     }
     if (number2 <= 2 || number2 >= 100){
-        document.getElementById("msg").innerHTML += "Number 1 input" + number2 + " is not a valid number.";
+        document.getElementById("msg2").innerHTML = "Number 1 input" + number2 + " is not a valid number.";
         document.getElementById("result").innerHTML = "";
-        document.getElementById("msg").style.color="red";
+        document.getElementById("msg2").style.color="red";
         document.getElementById("result").style.color="red";
+        document.getElementById("array").innerHTML="";
     }
     else if (number1 >= 2 && number1 <= 100 && number2 >= 2 && number2 <= 100){
         calculateEvenNum(number1,number2);
+        document.getElementById("msg1").innerHTML="";
+        document.getElementById("msg2").innerHTML="";
+
     }
 }
 function calculateEvenNum(Num1, Num2){
@@ -46,9 +50,9 @@ function calculateEvenNum(Num1, Num2){
     }
     console.log(eNumArray);
     document.getElementById("result").innerHTML = "There are " + eNumArray.length +" even numbers:";
-    document.getElementById("msg").innerHTML = eNumArray;
+    document.getElementById("array").innerHTML = eNumArray;
     document.getElementById("result").style.color="blue";
-    document.getElementById("msg").style.color="blue";
+    document.getElementById("array").style.color="blue";
 }
 
 function isEven(num) {
